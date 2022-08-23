450273735c5d7544879512d6143ebf0305213e53
chenjiyuan <chenjiyuan@bytedance.com>
2022-08-23 11:33:05 +0800

    modify the bellmanford.ts

--- tsTemp/08bellman-ford.ts
+++ tsTemp/08bellman-ford.ts
@@ -10,6 +10,8 @@ const edges: Array<edge> = Array(n).fill({a: 0, b: 0, w: 0});
 const dis: Array<number> = Array(n + 1).fill(Infinity);
 
 const bellmanford: (arg: number) => void = (k: number): void => {
+  dis[1] = 0;
+
   for (let i = 1; i <= k; i ++) {
     const backup: Array<number> = [...dis];
     for (let j = 0; j < n; j ++) {
